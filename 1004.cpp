98e4e49c88de270131d5ffac748aa55b10463111
Luiz <luizfrs@icloud.com>
2019-09-26 13:37:01 -0300

    Simple Product

---
+++ 1004.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int A, B, PROD;
+
+	cin>>A>>B;
+	PROD = A*B;
+
+	cout<<"PROD = "<<PROD<<endl;
+
+	return 0;
+}
